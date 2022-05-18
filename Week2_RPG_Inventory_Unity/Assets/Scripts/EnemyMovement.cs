5fb318429aa5cb36b26f87b36b76c2b6f6e936af
Blacka796 <davidshi796@gmail.com>
2022-05-17 22:28:18 -0400

    Final Version
    Final version for DGM6403 Experiment #2

---
+++ Week2_RPG_Inventory_Unity/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+    
+    public float speed;
+    public float stopDistance;
+    public float chaseRange;
+
+    public Transform target;
+
+    void Start()
+    {
+        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
+        if (distanceToTarget < chaseRange) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        } //Vector2.Distance(transform.position, target.position) > stopDistance)
+            
+
+        /*float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget < chaseRange)
+        {
+            Vector3 targetDir = target.position = transform.position;
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }*/
+
+    }
+}
---
+++ Week2_RPG_Inventory_Unity/Assets/Scripts/SunAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunAttack : MonoBehaviour
+{
+
+    private PlayerController player;
+    public int healthloss;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.name == "Player")
+        {
+            player.health -= healthloss;
+        }
+    }
+}
+
+  
